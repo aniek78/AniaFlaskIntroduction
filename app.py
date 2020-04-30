94548341a240a79f20283aecb67227fc18e7bd81
Ania Borg <aniaborg@Anias-MacBook-Air.local>
2020-04-30 20:29:12 +0100

    Redirects / to /calories

--- app.py
+++ app.py
@@ -1,37 +1,13 @@
 from flask import Flask, render_template, url_for, request, redirect
-from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
-
-
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return '<Task %r>' % self.id
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    if request.method == 'POST':
-        task_content = request.form['content']
-        new_task = Todo(content=task_content)
-
-        try:
-            db.session.add(new_task)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue adding your task'
-
-    else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
+    return redirect("/calories")
 
 
 def calculate_bmr(gender, weight, height, age, body_type):
@@ -55,7 +31,7 @@ def calories():
         weight = int(request.form['weight'])
         body_type = int(request.form['body_type'])
         bmr = calculate_bmr(gender, weight, height, age, body_type)
-        
+
         # - Strength training details ----------------------------------
         intensity = int(request.form['intensity'])
         frequency = int(request.form['frequency'])
@@ -64,17 +40,17 @@ def calories():
 
         # - Render the results page -----------------------------------
         html = render_template('results.html',
-                                age=age,
-                                body_type=body_type,
-                                duration=duration,
-                                frequency=frequency,
-                                gender=gender,
-                                height=height, 
-                                bmr=bmr,
-                                intensity=intensity,
-                                training=training,
-                                weight=weight
-                            )
+                               age=age,
+                               body_type=body_type,
+                               duration=duration,
+                               frequency=frequency,
+                               gender=gender,
+                               height=height,
+                               bmr=bmr,
+                               intensity=intensity,
+                               training=training,
+                               weight=weight
+                               )
 
         return html
     else:
