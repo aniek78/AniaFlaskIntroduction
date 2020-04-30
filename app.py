983b1ed88140fde8abae94d51414a8811578d990
Ania Borg <aniaborg@Anias-MacBook-Air.local>
2020-04-30 20:30:03 +0100

    Removes test db

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, url_for, request, redirect
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
 
 @app.route('/', methods=['POST', 'GET'])
